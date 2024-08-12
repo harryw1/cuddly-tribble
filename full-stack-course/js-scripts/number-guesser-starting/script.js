b5f91db078d0312145430088db6771063bdd87c0
Harry Weiss <harrisonrweiss1@gmail.com>
2024-08-12 19:56:48 -0400

    guessing game practice

--- full-stack-course/js-scripts/number-guesser-starting/script.js
+++ full-stack-course/js-scripts/number-guesser-starting/script.js
@@ -4,3 +4,28 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, target) => {
+    const humanDifference = Math.abs(target - humanGuess);
+    const computerDifference = Math.abs(target - computerGuess);
+    if (humanDifference <= computerDifference) {
+        return true;
+    } else if (humanDifference > computerDifference) {
+        return false;
+    }
+}
+
+const updateScore = (winner) => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}

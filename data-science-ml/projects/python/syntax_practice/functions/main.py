3ff9add131eb7dae0a749a5f9a4a6fbe9aa43c39
harryw1 <harrisonrweiss1@gmail.com>
2024-10-15 11:25:32 -0400

    removed unnecessary files

--- data-science-ml/projects/python/syntax_practice/functions/main.py
+++
@@ -1,7 +0,0 @@
-def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_time):
-    car_rental_total = car_rental_rate * trip_time
-    hotel_total = hotel_rate * trip_time - 10
-    total = plane_ticket_price + car_rental_total + hotel_total
-    return total
-
-print(calculate_expenses(200, 100, 100, 5))

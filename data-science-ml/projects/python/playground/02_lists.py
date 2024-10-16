59ef26c45134ac09dfacc01feb0e3e0f7870fa0b
harryw1 <harrisonrweiss1@gmail.com>
2024-10-16 14:19:45 -0400

    more project files for codeacademy

--- data-science-ml/projects/python/playground/02_lists.py
+++ data-science-ml/projects/python/playground/02_lists.py
@@ -50,3 +50,24 @@ ages = [["Aaron", 15], ["Dhruti", 16]]
 
 class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
 print(class_name_test)
+
+sams_score = class_name_test[2][1]
+print(sams_score)
+ellies_score = class_name_test[-1][-1]
+print(ellies_score)
+
+incoming_class = [["Kenny", "American", 9], ["Tanya", "Ukrainian", 9], ["Madison", "Indian", 7]]
+print(incoming_class)
+incoming_class[2][2] = 8
+print(incoming_class)
+incoming_class[-3][-3] = "Ken"
+print(incoming_class)
+
+"""
+    I really like this a lot! `zip`.
+"""
+
+names = ["a", "b", "c"]
+nums = [1,2,3]
+names_nums = zip(names,nums)
+print(names_nums)

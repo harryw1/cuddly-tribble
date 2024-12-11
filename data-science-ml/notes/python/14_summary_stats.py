f76a4eb1c7871d8f0a6f725a8f9416514270741d
Harry Weiss <harrisonrweiss1@gmail.com>
2024-12-11 12:21:49 -0500

    summary stats practice

--- data-science-ml/notes/python/14_summary_stats.py
+++ data-science-ml/notes/python/14_summary_stats.py
@@ -3,27 +3,102 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import scipy.stats as stats
 import seaborn as sns
 
 
 def main():
+    """Analyze student data with summary statistics."""
     # Load the data
     students = pd.read_csv("14_summary_stats.csv")
 
     # Display the first few rows of the data
-    print(students.head())
+    print(students.head(), "\n")
 
     # Summary statistics
-    print(students.describe())
+    print(f'Summary Stats:\n {students.describe(include="all")}', "\n")
 
     # Calculate the mean
-    print(students.math_grade.mean())
+    print(f"Mean: {students.math_grade.mean()}")
 
     # Calculate the median
-    print(students.math_grade.median())
+    print(f"Median: {students.math_grade.median()}")
 
     # Calculate the mode
-    print(students.math_grade.mode())
+    print(f"Mode: {students.math_grade.mode()}")
+
+    # Calculate the range
+    range = students.math_grade.max() - students.math_grade.min()
+    print(f"Range: {range}")
+
+    # Calculate mean absolute deviation
+    print(f"Mean Absolute Deviation: {mad(students.math_grade):.2f}")
+
+    # Alternatively, calculate median absolute deviation
+    print(
+        f"Median Absolute Deviation: {stats.median_abs_deviation(students.math_grade):.2f}"
+    )
+
+    # Visualize the data in a histogram
+    # histvisual(students.math_grade)
+
+    # Visualize the data in a boxplot
+    # boxplot(students.math_grade)
+
+    # Summary of mothers' jobs
+    print(f"Frequency of mother's jobs: \n{students.Mjob.value_counts()}")
+
+    # Mother's job as a percentage
+    print(
+        f"Mother's job as a percentage: \n{students.Mjob.value_counts(normalize=True)}"
+    )
+
+    # Summary of father's jobs
+    print(f"Frequency of father's jobs: \n{students.Fjob.value_counts()}")
+
+    # Father's job as a percentage
+    print(
+        f"Father's job as a percentage: \n{students.Fjob.value_counts(normalize=True)}"
+    )
+
+    # Countplot of mother jobs
+    countplot(students.Mjob)
+
+    # Pie chart of mother jobs
+    piechart(students.Mjob)
+
+
+def mad(data):
+    """Calculate the mean absolute deviation."""
+    return np.mean(np.abs(data - np.mean(data)))
+
+
+def histvisual(data):
+    """Return a histogram and a boxplot of the data."""
+    sns.histplot(data)
+    plt.show()
+    plt.close()
+
+
+def boxplot(data):
+    """Return a boxplot of the data."""
+    sns.boxplot(data)
+    plt.show()
+    plt.close()
+
+
+def countplot(data):
+    """Return a countplot of the data."""
+    sns.countplot(data)
+    plt.show()
+    plt.close()
+
+
+def piechart(data):
+    """Return a pie chart of the data."""
+    data.value_counts().plot.pie()
+    plt.show()
+    plt.close()
 
 
 if __name__ == "__main__":

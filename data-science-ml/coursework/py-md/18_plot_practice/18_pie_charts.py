39bcecd64f04d2876d95e0c22e427706f735256f
Harry Weiss <harrisonrweiss1@gmail.com>
2025-01-19 11:20:06 -0500

    Update 18_pie_charts.py

--- data-science-ml/coursework/py-md/18_plot_practice/18_pie_charts.py
+++ data-science-ml/coursework/py-md/18_plot_practice/18_pie_charts.py
@@ -8,3 +8,14 @@ payment_method_freqs = [270, 77, 32, 11]
 plt.pie(payment_method_freqs)
 plt.axis('equal')
 plt.show()
+
+# or making a pie chart with labels and a legend
+payment_method_names = ["Card Swipe", "Cash", "Apple Pay", "Other"]
+payment_method_freqs = [270, 77, 32, 11]
+
+# string formatting to add percentages to the pie chart too
+plt.pie(payment_method_freqs, autopct='%0.1f%%')
+plt.axis('equal')
+plt.legend(payment_method_names)
+
+plt.show()

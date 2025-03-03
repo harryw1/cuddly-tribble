72f45a7f427017952b50d5d3ba1d2a04a465c863
Harry Weiss <harrisonrweiss1@gmail.com>
2025-03-02 21:31:54 -0500

    testing fleet

--- data-science-ml/coursework/py-md/25_calculus/practice.py
+++ data-science-ml/coursework/py-md/25_calculus/practice.py
@@ -1,6 +1,6 @@
 import sympy as sp
 
-x = sp.symbols("x")
+x = sp.symbols('x')
 f = 2 * sp.ln(x) - 6
 limit = sp.limit(f, x, 1)
 print(limit)
@@ -9,7 +9,7 @@ print(limit)
 x = sp.symbols('x')
 
 # Define the function f(x) = 2x**4 + e**x - sin(x)
-f_x = 2*x**4 + sp.exp(x) - sp.sin(x)
+f_x = 2 * x ** 4 + sp.exp(x) - sp.sin(x)
 
 # Calculate the derivative of f(x) with respect to x
 derivative_f_x = sp.diff(f_x, x)
